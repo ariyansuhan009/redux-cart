7b34591e38697b864d27f58a25af45fbeee78c0a
ariyansuhan009 <ariyansuhan009@gmail.com>
2021-10-13 16:07:02 +0000

    cart done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { addToCart } from '../../redux/actions/cartAction';
+import Product from '../Product/Product';
+import {connect} from 'react-redux';
+
+const Shop = (props) => {
+     
+     const { products, addToCart } = props;
+     return (
+          <div>
+               <h2>This is Shop</h2>
+               {
+                    products.map( pd => <Product 
+                         product = {pd}
+                         key = {pd.id}
+                         addToCart = {addToCart}
+                         ></Product>)
+               }
+          </div>
+     );
+};
+
+const mapStateToProps = state => {
+     return {
+          products: state.products
+     }
+}
+
+const mapDispatchToProps = {
+     addToCart: addToCart
+}
+
+/* const connectToStore = connect(mapStateToProps, mapDispatchToProps);
+
+connectToStore(Shop); */
+
+export default connect(mapStateToProps, mapDispatchToProps) (Shop);
---
+++ src/redux/store/store.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+import cartReducers from '../reducer/cartReducers';
+
+
+export const store = createStore(cartReducers);
